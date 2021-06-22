5bc2eb9cdf02384db6f078525361b4b37564fd04
Caleb Odinga <calemasanga@gmail.com>
2021-06-22 17:06:31 +0300

    adds model and table relationships in models.py

--- app/models.py
+++ app/models.py
@@ -1,54 +1,45 @@
+from sqlalchemy.orm import lazyload
 from . import db
 from flask_login import UserMixin
+import datetime
 
-class bookAppointment(UserMixin, db.Model):
-    """User booking model."""
+class User(UserMixin, db.Model):
+    __tablename__='users'
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    role = db.Column(db.Integer, db.ForeignKey('roles.role_id'))
+    appointments = db.Column(db.Integer, db.ForeignKey('appointments.appointment_id'))
+    posts_added = db.Column(db.Integer, db.ForeignKey('posts.post_id'))
 
-    __tablename__ = 'booking'
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
-    name = db.Column(
-        db.String(100),
-        nullable=False,
-        unique=False
-    )
 
-    phone = db.Column(
-        db.Integer(100),
-        nullable=False,
-        unique=False
-    )
+class Role(db.Model):
+    __tablename__='roles'
+    role_id = db.Column(db.Integer, primary_key = True)
+    role = db.Column(db.String(100))
+    user = db.relationship('User', backref='role', lazy='dynamic')
 
-    email = db.Column(
-        db.String(40),
-        nullable=False,
-        unique=False
-    )
+class Post(db.Model):
+    __tablename__='posts'
+    post_id = db.Column(db.Integer, primary_key = True)
+    post_title = db.Column(db.String(50))
+    house_price = db.Columne(db.String(255))
+    house_location = db.Column(db.String(100))
+    photo_path = db.Column(db.String())
+    user = db.relationship('User', backref = 'posts', lazy='dynamic')
+    category = db.Column(db.Integer, db.ForeignKey('categories.category_id'))
 
-class posts(UserMixin, db.Model):
-    """User booking model."""
-    
-    __tablename__ = 'posts'
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
-    name = db.Column(
-        db.String(100),
-        nullable=False,
-        unique=False
-    )
+class Category(db.Model):
+    __tablename__='categories'
+    category_id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(100))
+    posts = db.relationship('Post', backref = 'posts', lazy='dynamic')
 
-    description = db.Column(
-        db.Integer(100),
-        nullable=False,
-        unique=False
-    )
-
-    photo = db.Column(
-        db.LargeBinary(40),
-        nullable=False,
-        unique=False
-    )
+class Appointment(db.Model):
+    __tablename__= 'appointments'
+    appointment_id = db.Column(db.Integer, primary_key = True)
+    appointment_title = db.Column(db.String(100))
+    appointment_content = db.COlumn(db.String(255))
+    appointment_date = db.Column(db.DateTime())
+    appointment_made_on = db.Column(db.DateTime(), default = datetime.utcnow)
+    user = db.relationship('User', backref='appointment', lazy='dynamic')
