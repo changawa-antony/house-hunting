01f24b87d7d2393e32e803b530649d57be6a33a7
sngina <sngina707@gmail.com>
2021-06-23 15:07:51 +0300

    installed flask login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,42 @@
-from flask import Flask
-from flask_mail import Message
-from flask import render_template
-from . import mail
+from flask import render_template,redirect,url_for,flash,request
+from flask_login import login_user ,logout_user, login_required
+from ..models import User
+from .forms import LoginForm, RegistrationForm
+from .. import db
 
 
-def mail_message(subject,template,to,**kwargs):
-    sender_email = "stella.kimaru@student.moringaschool.com"
 
-    email = Message(subject, sender=sender_email, recipients=[to])
-    email.body= render_template(template + ".txt",**kwargs)
-    email.html = render_template(template + ".html",**kwargs)
-    mail.send(email)
+from .import auth
+@auth.route('/login',methods = ['GET' , 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "watchlist login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
+
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        # mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+    title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,11 @@
 from . import main
 from flask import render_template
+from flask_login import login_user ,logout_user, login_required
+
+
 
 @main.route('/login')
-
+@login_required
 def index ():
       
-  return render_template('auth/login.html')
+  return render_template('index.html')
