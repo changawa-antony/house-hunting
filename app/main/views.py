de6dbc01095972d78734a489b8ac820602388476
Caleb Odinga <calemasanga@gmail.com>
2021-06-23 17:03:21 +0300

    adds a form for booking appointment

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,11 @@
 from . import main
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_required
+from ..main.forms import BookApp
+from ..models import User, Appointment
 
 @main.route('/')
 def index ():
       
   return render_template('index.html')
+
