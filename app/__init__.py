b7e1d74810a8d6ccb0f089b6fc8eb0f38b41d863
sngina <sngina707@gmail.com>
2021-06-23 14:39:58 +0300

    pip install Werkzeug

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
-from flask_mail import Mail
+from flask_mail import _Mail
 
-simple = SimpleMDE()
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
