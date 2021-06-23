2d1fb3501650ac47ee09477d52cf58116ea1b7df
sngina <sngina707@gmail.com>
2021-06-23 11:33:07 +0300

    add forms.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,13 @@
 from flask import Flask
-from flask.templating import render_template
-from . import main
+from flask_mail import Message
+from flask import render_template
+from . import mail
 
-@main.route('/')
-def index():
-    render_template('index.html')
+
+def mail_message(subject,template,to,**kwargs):
+    sender_email = "stella.kimaru@student.moringaschool.com"
+
+    email = Message(subject, sender=sender_email, recipients=[to])
+    email.body= render_template(template + ".txt",**kwargs)
+    email.html = render_template(template + ".html",**kwargs)
+    mail.send(email)
