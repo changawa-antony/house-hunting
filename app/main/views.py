ec92bdbfd90db22f39cf2b5da1ea4bb2a84e12a0
changawa-antony <antony.changawa@student.moringaschool.com>
2021-06-24 08:58:57 +0300

    front-end design

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,27 @@
+from flask.helpers import url_for
 from . import main
 from flask import render_template
 
 @main.route('/')
-def index ():
+def index():
+      
       
   return render_template('index.html')
+
+@main.route('/appointment')
+def appointment():
+      
+      
+  return render_template('appointment.html')
+
+@main.route('/sell')
+def sell():
+      
+      
+  return render_template('sell.html')
+
+@main.route('/gallery')
+def gallery():
+      
+      
+  return render_template('gallery.html')
