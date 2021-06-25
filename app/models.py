61e2ff18227fbb13ac84535a22d825b8631d0619
Caleb Odinga <calemasanga@gmail.com>
2021-06-25 03:20:18 +0300

    adds changes to models, table apointments

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100))
     password = db.Column(db.String())
     user_role = db.Column(db.Integer, db.ForeignKey('roles.role_id'))
-    appointments = db.Column(db.Integer, db.ForeignKey('appointments.appointment_id'))
+    appointments = db.relationship('Appointment', backref='appointments_made', lazy = 'dynamic')
     posts_added = db.Column(db.Integer, db.ForeignKey('posts.post_id'))
 
     def verify_password(self, password):
@@ -53,4 +53,4 @@ class Appointment(db.Model):
     appointment_content = db.Column(db.String(255))
     appointment_date = db.Column(db.DateTime())
     appointment_made_on = db.Column(db.DateTime(), default = datetime.utcnow)
-    user = db.relationship('User', backref='appointment', lazy='dynamic')
+    user_appointee = db.Column(db.Integer, db.ForeignKey('users.user_id'))
